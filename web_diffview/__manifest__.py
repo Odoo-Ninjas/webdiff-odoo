3ba0529bf02af0f6303ffeeebd6e6b0c07bc95d0
Marc Wimmer <marc@zebroo.de>
2024-07-01 09:21:18 +0200

    no less

--- web_diffview/__manifest__.py
+++ web_diffview/__manifest__.py
@@ -1,12 +1,21 @@
-{   'application': False,
-    'author': 'Marc Wimmer (marc@itewimmer.de)',
-    'css': ['static/diffview.less', 'static/res/style.css'],
-    'data': ['views/assets.xml'],
-    'demo': [],
-    'depends': ['web'],
-    'external_dependencies': {'bin': [], 'python': []},
-    'name': 'web_diffview',
-    'qweb': ['static/src/diffview.xml'],
-    'test': [],
-    'version': '1.0',
-    'web': True}
+{
+    "application": False,
+    "assets": {
+        "web.assets_backend": [
+            "web_diffview/static/src/diffview.css",
+            "web_diffview/static/res/style.css",
+            "web_diffview/static/src/diffview.js",
+            "web_diffview/static/res/diff.js",
+            "web_diffview/static/src/diffview.xml",
+        ],
+    },
+    "author": "Marc Wimmer (marc@zebroo.de)",
+    "data": [],
+    "demo": [],
+    "depends": ["web"],
+    "external_dependencies": {"bin": [], "python": []},
+    "name": "web_diffview",
+    "test": [],
+    "version": "1.0",
+    "web": True,
+}
